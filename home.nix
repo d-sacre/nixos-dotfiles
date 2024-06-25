88cd73c0822770af350d7479ff572367ed18a704
d-sacre <daniel.sacre@gmx.de>
2024-06-25 17:33:16 +0200

    changed extensions for vscodium

--- home.nix
+++ home.nix
@@ -42,9 +42,8 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
-      dracula-theme.theme-dracula
-      vscodevim.vim
-      yzhang.markdown-all-in-one
+      bbenoist.nix
+      ms-python.python
     ];
   };
 
